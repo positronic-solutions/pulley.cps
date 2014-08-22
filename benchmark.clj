d15a2e177b6c621138308290b8757571da6d6f72
Nathan Davis <ndavis@positronic-solutions.com>
2014-08-22 17:11:25 -0500

    Add benchmarking script

---
+++ benchmark.clj
@@ -0,0 +1,34 @@
+(require '[com.positronic-solutions.cps :as cps])
+(require '[criterium.core :as criterium])
+
+(defn benchmark-factorial [n]
+  (println "BENCHMARK factorial-recur")
+  (criterium/bench (cps/factorial-recur n))
+
+  (println "BENCHMARK factorial-loop")
+  (criterium/bench (cps/factorial-loop n))
+
+  (println "BENCHMARK factorial-cps1")
+  (criterium/bench (cps/factorial-cps1 n))
+
+  (println "BENCHMARK factorial-cps2")
+  (criterium/bench (cps/factorial-cps2 n))
+
+  (println "BENCHMARK factorial-cps3")
+  (criterium/bench (cps/factorial-cps3 n)))
+
+(defn benchmark-function [f arglists]
+  (let [separator (apply str (repeat 40 "="))]
+    (doseq [args arglists]
+      (println separator)
+      (println "args: " args)
+      (println separator)
+
+      (apply f args)
+      (println))))
+
+(defn run-benchmarks []
+  (benchmark-function benchmark-factorial
+                      [[10] [20] [10N] [2543N]]))
+
+(run-benchmarks)
--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [criterium "0.4.3"]]
+  :plugins [[lein-exec "0.3.4"]]
+  ;; disable tiered compilation for benchmarks
+  :jvm-opts ^:replace [])
