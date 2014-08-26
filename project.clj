b7c81c857689f04c3b5411906cff5b622c18c521
Nathan Davis <ndavis@positronic-solutions.com>
2014-08-26 15:08:46 -0500

    Add project description

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
 ;; All rights reserved.
 
 (defproject com.positronic-solutions.cps "0.1.0-SNAPSHOT"
+  :description "CPS transformation macros and trampoline execution engine"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [criterium "0.4.3"]]
   :plugins [[lein-exec "0.3.4"]]
