6e281dba4a4a82b062e47e5b176430d4e569e40f
Nathan Davis <ndavis@positronic-solutions.com>
2014-08-26 15:07:10 -0500

    Removed unused / erroneous items from project.clj

--- project.clj
+++ project.clj
@@ -2,10 +2,6 @@
 ;; All rights reserved.
 
 (defproject com.positronic-solutions.cps "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [criterium "0.4.3"]]
   :plugins [[lein-exec "0.3.4"]]
