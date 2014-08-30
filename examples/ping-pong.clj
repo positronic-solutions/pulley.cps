9627a25ab773d9dbb0cec97d0994253966d9f12e
Nathan Davis <ndavis@positronic-solutions.com>
2014-08-29 22:37:35 -0500

    Add "ping-pong" example

---
+++ examples/ping-pong.clj
@@ -0,0 +1,31 @@
+(require '[com.positronic-solutions.cps :as cps])
+
+(declare pong)
+
+(def ping
+  (cps/cps-fn ([n]
+                 (if (> n 0)
+                   (do (println "ping!")
+                       (pong (dec n)))
+                   (println "ping failed to return the volley!")))))
+
+(def pong
+  (cps/cps-fn ([n]
+                 (if (> n 0)
+                   (do (println "pong!")
+                       (ping (dec n)))
+                   (println "pong failed to return the volley!")))))
+
+(declare cps-odd?)
+
+(def cps-even?
+  (cps/cps-fn ([n]
+                 (if (> n 0)
+                   (cps-odd? (dec n))
+                   true))))
+
+(def cps-odd?
+  (cps/cps-fn ([n]
+                 (if (> n 0)
+                   (cps-even? (dec n))
+                   false))))
