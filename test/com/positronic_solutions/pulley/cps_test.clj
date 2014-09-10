530645814c8a164d79ed61ecb20293fd9bbd005a
Nathan Davis <ndavis@positronic-solutions.com>
2014-09-10 13:37:27 -0500

    Add some tests

---
+++ test/com/positronic_solutions/pulley/cps_test.clj
@@ -0,0 +1,121 @@
+(ns com.positronic-solutions.pulley.cps-test
+  (:use clojure.test
+        [clojure.pprint :only [pprint]]
+        com.positronic-solutions.pulley.cps))
+
+(def ^:dynamic *foo* 0)
+(def ^:dynamic *bar* 0)
+
+(defmacro verify-form-equiv
+  "Macro for testing that a given input form evaluates to the same value,
+regardless of whether it is passed through the CPS compiler.
+
+Basically, two copies of the form will be created.
+One will be passed through the CPS compiler, the other will not.
+Both forms will then be evaluated and the resulting values compared
+to ensure they are equivalent."
+  ([form]
+     `(testing (with-out-str (pprint ~form))
+        (is (= ~form
+               (cps ~form))))))
+
+(deftest test-atomic-expression
+  (without-recursive-trampolines
+   (with-strict-cps
+     (verify-form-equiv 1)
+     (verify-form-equiv "Hello, world!")
+     (let [x 1]
+       (verify-form-equiv x)))))
+
+(deftest test-binding
+  (without-recursive-trampolines
+   (with-strict-cps
+     (verify-form-equiv (binding [*foo* 2]
+                          *foo*))
+     (verify-form-equiv (do (binding [*foo* 2])
+                            *foo*)))))
+
+(deftest test-do
+  (without-recursive-trampolines
+   (with-strict-cps
+     (verify-form-equiv (do 1 1 1)))
+   (verify-form-equiv (do (+ 1 2)
+                          (+ 2 3)
+                          (+ 3 4)))))
+
+(deftest test-fn
+  (without-recursive-trampolines
+   (with-strict-cps
+     (verify-form-equiv ((fn [] 10)))
+     (verify-form-equiv ((fn [x] x) 10)))
+   (verify-form-equiv ((fn [x y]
+                         (+ x y))
+                       1 2))
+   (verify-form-equiv ((fn [& xs]
+                         (reduce + xs))
+                       10 11 12 13)))
+  (verify-form-equiv (apply (fn [& xs]
+                              (reduce * xs))
+                            (range 1 10))))
+
+(deftest test-if
+  (without-recursive-trampolines
+   (with-strict-cps
+     (verify-form-equiv (if true :a :b))
+     (verify-form-equiv (if false :a :b)))
+   (verify-form-equiv (if (symbol? 'x) :a :b))
+   (verify-form-equiv (if (symbol? 1) :a :b))
+   (verify-form-equiv (if (symbol? 'x)
+                        (+ 1 2)
+                        (* 3 4)))
+   (verify-form-equiv (if (symbol? 1)
+                        (+ 1 2)
+                        (* 3 4)))))
+
+(deftest test-let
+  (without-recursive-trampolines
+   (with-strict-cps
+     (verify-form-equiv (let [x 1]
+                          x))
+     (verify-form-equiv (let [x 5]
+                          (let [x 3]
+                            x)))
+     (verify-form-equiv (let [x 5]
+                          (let [x 3])
+                          x))
+     (verify-form-equiv (let [x 5]
+                          (let [x 3]
+                            x)
+                          x)))
+   (verify-form-equiv (let [x 5
+                            y (+ x 4)
+                            x (+ x y 3)]
+                        x))))
+
+(deftest test-letfn
+  (without-recursive-trampolines
+   (with-strict-cps
+     (verify-form-equiv (letfn [(foo [] 10)]
+                          (foo))))
+   (verify-form-equiv (letfn [(even? [x]
+                                (if (= x 0)
+                                  true
+                                  (odd? (dec x))))
+                              (odd? [x]
+                                (if (= x 0)
+                                  false
+                                  (even? (dec x))))]
+                        (map even? (range 10))))))
+
+(deftest test-quote
+  (without-recursive-trampolines
+   (with-strict-cps
+     (verify-form-equiv 'x)
+     (verify-form-equiv '(x y z))
+     (verify-form-equiv '(x (y) z)))))
+
+(deftest test-var-form
+  (without-recursive-trampolines
+   (with-strict-cps
+     (verify-form-equiv (var *foo*))
+     (verify-form-equiv (var deftest)))))
